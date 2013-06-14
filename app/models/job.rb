49c43986ed532fc16a4586387a7b96f37ed6c4f4
George Gray <george@ummodesign.com>
2013-06-14 15:07:35 +0100

    hustling

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,5 @@
 class Job < ActiveRecord::Base
-  attr_accessible :company, :company_logo, :company_url, :created_at, 
+  attr_accessible :company, :company_logo, :company_url, :created_at,
   :description, :github_id, :how_to_apply, :location, :title, :type, :url
 
   belongs_to :user
